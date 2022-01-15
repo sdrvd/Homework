c55b10ace12bcb6593f319ffe3e66fb2f266210a
unknown <danpin@tut.by>
2022-01-15 22:51:55 +0300

    №2

--- home_work_4/container/DataContainer.java
+++ home_work_4/container/DataContainer.java
@@ -2,14 +2,17 @@ package home_work_4.container;
 
 
 
-import java.util.Arrays;
 
-public class DataContainer<E> {
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class DataContainer<E>{
 
 
     private E[] data;
-    //private E[] innerData;
     private int index = 0;
+    private E[] innerData;
 
     public DataContainer(E[] data) {
         this.data = data;
@@ -17,36 +20,108 @@ public class DataContainer<E> {
 
 
     public int add(E item){
+        int counter = 0;
+
+        if(data.length == 0){
+            data = Arrays.copyOf(data, 1);
+        }
 
         if (data[index] == null){
             data[index] = item;
             index++;
         }
 
+
         if(index >= data.length) {
             data = Arrays.copyOf(data, index+1);
         }
+
 
         return index-1;
     }
 
     public E get(int index){
-        return data[index];
+        return data[index-1];
     }
 
     public E[] getItems(){
-        return data;
+        return Arrays.copyOf(data, data.length);
     }
 
-    /*boolean delete(int position){
-        if(data[position] != null){
-            data[position] = data[position+1];
-            data = Arrays.copyOf(data, index-1);
-            return true;
-        }
-        if(data[position] == null){
+    public boolean deletePosition(int position){
+        int counter = 0;
+
+        if(index < 0 || index >= data.length){
             return false;
         }
+
+        for (int i = position; i < data.length; i++) {
+            data[i - 1] = data[i];
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            if(data[i] == null){
+                counter++;
+            }
+        }
+
+
+        return true;
+    }
+
+   public boolean deleteItem(E item){
+
+
+       for (int i = 0; i < data.length; i++) {
+          if(data[i].equals(item)){
+              deletePosition(i+1);
+              return true;
+          }
+       }
+
+       return false;
+    }
+
+    @Override
+    public String toString() {
+        int counter = 0;
+
+        for (int i = 0; i < data.length; i++) {
+            if(data[i] == null){
+                counter++;
+            }
+        }
+
+        innerData = Arrays.copyOf(data, data.length-counter);
+
+        for (int i = 0; i < data.length; i++) {
+            if(data[i] != null){
+                innerData[i] = data[i];
+            }
+        }
+
+        return "data = " + Arrays.toString(innerData);
+    }
+
+   /*public void sort(Comparator<E> comparator){
+        for (int i = 0; i < data.length;) {
+            boolean d1 = false;
+            for (int k = i+1; k < data.length && !d1; k++) {
+            if(data[i] != null && data[k] != null) {
+                d1 = comparator.compare(data[i], data[k]) > 0;
+                if (d1) {
+                    E tmp = data[k];
+                    data[k] = data[i];
+                    data[i] = tmp;
+                }
+                if (!d1) {
+                    i++;
+                }
+            }
+            }
+        }
     }*/
+
+
 
 }
--- home_work_4/run/DataContainerMain.java
+++ home_work_4/run/DataContainerMain.java
@@ -1,27 +1,39 @@
 package home_work_4.run;
 
+import home_work_4.comparators.IntegerComparator;
 import home_work_4.container.DataContainer;
+
+import java.util.Arrays;
 
 public class DataContainerMain {
     public static void main(String[] args) {
-        Integer[] beta = new Integer[1];
+        Integer[] beta = new Integer[0];
         DataContainer<Integer> cont = new DataContainer<>(beta);
         System.out.println(cont.add(15));
         System.out.println(cont.add(55));
         System.out.println(cont.add(62));
         System.out.println(cont.add(99));
         System.out.println(cont.add(92));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.add(501));
-        System.out.println(cont.get(2));
-        System.out.println(cont.getItems());
+        System.out.println(cont.add(503));
+        System.out.println(cont.add(52225));
+        System.out.println(cont.add(504));
+        System.out.println(cont.add(506));
+        System.out.println(cont.add(5999));
+        System.out.println(cont.add(509));
+        System.out.println(cont.add(510));
+        System.out.println(cont.add(515));
+        System.out.println(cont.add(522));
+        System.out.println(cont.add(599));
+        System.out.println(cont.toString());
+        System.out.println("№1 = " + cont.get(2));
+        System.out.println(cont.deletePosition(2));
+        System.out.println("№2 = " + cont.get(2));
+        System.out.println("№3 = " + cont.get(3));
+        System.out.println(cont.deleteItem(99));
+        System.out.println(cont.deleteItem(510));
+        System.out.println(Arrays.toString(cont.getItems()));
+        System.out.println(cont.toString());
+        /*cont.sort(new IntegerComparator());
+        System.out.println(cont.toString());*/
     }
 }
